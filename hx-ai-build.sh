d06a17c7720e3bce05979a63b4857b9050dda4e4
Shaun Keys <mariobuddy@gmail.com>
2026-02-25 18:35:53 -0700

    Update configs & add multi-arch support

--- hx-ai-build.sh
+++ hx-ai-build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+ARCH="$(uname -m)"
+case "$ARCH" in
+	x86_64)
+		NODE_ARCH="x64"
+		;;
+	aarch64)
+		NODE_ARCH="arm64"
+		;;
+	*)
+		echo "Your architecture is not supported"
+		exit 1
+esac
+
 cd "$(dirname "$0")"
 MAIN_DIR="$(pwd)"
 
@@ -7,8 +20,8 @@ mkdir -p approot/{bin,lib,opt,usr/lib/helix}
 
 # Install nodejs
 NODE_VER='v20.14.0'
-rm -rf "approot/node-$NODE_VER-linux-x64"
-wget -O- "https://nodejs.org/dist/$NODE_VER/node-$NODE_VER-linux-x64.tar.xz" | tar -JxvC approot
+rm -rf "approot/node-$NODE_VER-linux-$NODE_ARCH"
+wget -O- "https://nodejs.org/dist/$NODE_VER/node-$NODE_VER-linux-$NODE_ARCH.tar.xz" | tar -JxvC approot
 
 # Install helix
 if [[ ! -d helix-editor ]]; then
@@ -21,6 +34,7 @@ fi
 	cd ..
 	cp -r runtime ../approot/usr/lib/helix/runtime 
 	cp -r ../config.toml ../approot/usr/lib/helix/config.toml
+	cp -r ../languages.toml ../approot/usr/lib/helix/languages.toml
 	rm -rf ../approot/usr/lib/helix/runtime/grammars/sources
 	cp target/release/hx ../approot/bin/hx
 	cp contrib/Helix.desktop ../approot/Helix.desktop
@@ -30,26 +44,26 @@ fi
 (
 	echo '#!/bin/bash'
 	echo 'APPDIR="$(dirname "$(readlink -f "${0}")")"'
-	echo "HELIX_RUNTIME=\"\$APPDIR/usr/lib/helix/runtime\" PATH=\"\$APPDIR/bin:\$APPDIR/node-$NODE_VER-linux-x64/bin:\$PATH\" \"\$APPDIR/bin/hx\" -c \"\$APPDIR/usr/lib/helix/config.toml\" \"\$@\""
+	echo "HELIX_RUNTIME=\"\$APPDIR/usr/lib/helix/runtime\" HELIX_CONFIG_DIR=\"$APPDIR/usr/lib/helix\" PATH=\"\$APPDIR/bin:\$APPDIR/node-$NODE_VER-linux-$NODE_ARCH/bin:\$PATH\" \"\$APPDIR/bin/hx\" \"\$@\""
 ) > approot/AppRun
 chmod +x approot/AppRun
 
 
-export PATH="$(pwd)/approot/bin:$(pwd)/approot/node-$NODE_VER-linux-x64/bin:$PATH"
-"./approot/node-$NODE_VER-linux-x64/bin/npm" i --prefix "./approot/node-$NODE_VER-linux-x64/" -g \
+export PATH="$(pwd)/approot/bin:$(pwd)/approot/node-$NODE_VER-linux-$NODE_ARCH/bin:$PATH"
+"./approot/node-$NODE_VER-linux-$NODE_ARCH/bin/npm" i --prefix "./approot/node-$NODE_VER-linux-$NODE_ARCH/" -g \
 	pyright vscode-langservers-extracted typescript typescript-language-server \
 	@vue/language-server yaml-language-server@next svelte-language-server \
 	dockerfile-language-server-nodejs @microsoft/compose-language-service bash-language-server \
 	@ansible/ansible-language-server perlnavigator-server intelephense awk-language-server emmet-ls
-rm -r approot/node-$NODE_VER-linux-x64/include
+rm -r approot/node-$NODE_VER-linux-$NODE_ARCH/include
 
 test -f approot/bin/rhai || \
 	cargo install --git https://github.com/rhaiscript/rhai-lsp.git --rev 2f1fcd73f43b909d1d5e96123516e599b9aaaa88 rhai-cli --root approot
 test -f approot/bin/ruff || \
 	cargo install --git https://github.com/astral-sh/ruff.git --tag 0.9.4 ruff --root approot
 
-if [[ ! -e appimagetool-x86_64.AppImage ]]; then
-	wget https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage
-	chmod +x appimagetool-x86_64.AppImage
+if [[ ! -e "appimagetool-$ARCH.AppImage" ]]; then
+	wget "https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-$ARCH.AppImage"
+	chmod +x "appimagetool-$ARCH.AppImage"
 fi
-./appimagetool-x86_64.AppImage approot/ hx-ai
+"./appimagetool-$ARCH.AppImage" approot/ hx-ai
